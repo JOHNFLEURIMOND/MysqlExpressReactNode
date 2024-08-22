d5a4070de59c4d82fa4e5a1f91c8c446ff616df6
John Fleurimond <johncfleurimond@gmail.com>
2024-08-22 17:24:34 -0400

    Update Netlify build command

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Import createRoot from 'react-dom/client'
 import Index from './Index.jsx'; // Assuming your main component is now Index
-import GlobalStyle from './GlobalStyle';
+import GlobalStyle from './GlobalStyle.jsx';
 import { ThemeProvider } from 'styled-components';
-import FleurimondTheme from './theme';
+import FleurimondTheme from './theme.jsx';
 import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root element
